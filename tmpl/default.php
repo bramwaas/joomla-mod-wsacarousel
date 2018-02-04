294ddacba45e194c8318fabf38b5432d254ceef2
Bram Waasdorp <bram@waasdorpsoekhan.nl>
2018-02-04 08:52:36 +0100

    0.0.0 jQuery verbeterd voor twee naast elkaar, 1 en meer dan twee moet nog.

--- tmpl/default.php
+++ tmpl/default.php
@@ -85,13 +85,22 @@ jQuery('.carousel .item').each(function(){
     next = jQuery(this).siblings(':first');
   }
   next.children(':first-child').clone().appendTo(jQuery(this));
-  
-  if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo(jQuery(this));
+  "
+/*
+ .
+  "
+  for (var i=0;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = jQuery(this).siblings(':first');
+  	}
+    
+    next.children(':first-child').clone().appendTo(jQuery(this));
   }
-  else {
-  	jQuery(this).siblings(':first').children(':first-child').clone().appendTo(jQuery(this));
-  }
+  "
+*/	
+.	
+"	
 });
 })
 ";
