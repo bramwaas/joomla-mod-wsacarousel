ed8a2aed15cb0c00483684c578693c2dfcdf467e
a.waasdorp <a.waasdorp@192.168.1.14>
2018-02-08 20:41:44 +0100

    0.0.2

--- mod_wsacarousel.php
+++ mod_wsacarousel.php
@@ -128,7 +128,7 @@ if ($jquery) {
 	        $document->addScript("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js", array('version'=>'1.12.9'),
 	            array('id'=>'popper.js' /* , 'integrity'=>'sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' */));
 	        $document->addScript("https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" , array('version'=>'4.0.0'), 
-	            array('id'=>'bootstrap.min.js', 'integrity' => 'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl', 'defer'=>'defer')); // defer .
+	            array('id'=>'bootstrap.min.js' /* , 'integrity' => 'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl' */ , 'defer'=>'defer')); // defer .
             $document->addScript(Uri::root(true)."/modules/mod_wsacarousel/assets/js/wsacarousel_bootstrap4.0.js", array('version'=>'4.0.0'),
                 array('id'=>'wsacarousel_bootstrap.js', 'defer'=>'defer')); // defer .
 	    }
