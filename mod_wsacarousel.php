0718fca04118a0b2e813abb4240274ece83ffb6c
bram.waasdorp <bram.waasdorp@FNV-LT10086.FNV.NET>
2018-02-09 08:27:06 +0100

    0.0.3

--- mod_wsacarousel.php
+++ mod_wsacarousel.php
@@ -105,14 +105,14 @@ $ver = $ver->version;
 
 if ($jquery) {
 	HTMLHelper::_('jquery.framework');
-	$document->addScript(dirname(__FILE__).'/media/djextensions/jquery-easing/jquery.easing.min.js', array('id'=>'jquery.easing.min.js', 'defer'=>'defer'));
+	$document->addScript(Uri::root(true).'/media/djextensions/jquery-easing/jquery.easing.min.js', array('id'=>'jquery.easing.min.js', 'defer'=>'defer'));
 	if ($params->get('twbs_version',4) == "3") {
 	    if ($params->get('include_twbs_css') == "1") {
 	   $document->addStyleSheet('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', array('version'=>'3.3.7'),
 	       array('id'=>'bootstrap.min.css', 'integrity' => 'sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u', 'crossorigin' => 'anonymous'));
 	    }
 	    if ($params->get('include_twbs_js') == "1") {
-	        $document->addScript(dirname(__FILE__)."/assets/js/wsacarousel_bootstrap3.3.7.js", array('version'=>'3.3.7'),
+	        $document->addScript(Uri::root(true)."/modules/mod_wsacarousel/assets/js/wsacarousel_bootstrap3.3.7.js", array('version'=>'3.3.7'),
 	            array('id'=>'wsacarousel_bootstrap.js', 'defer'=>'defer')); // defer .
 	    }
 	}
@@ -124,7 +124,7 @@ if ($jquery) {
 	    if ($params->get('include_twbs_js') == "1") {
 	        $document->addScript("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js", array('version'=>'1.12.9'),
 	            array('id'=>'popper.js' /* , 'integrity'=>'sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q' */));
-	        $document->addScript(dirname(__FILE__)."/assets/js/wsacarousel_bootstrap4.0.js", array('version'=>'4.0.0'),
+	        $document->addScript(Uri::root(true)."/modules/mod_wsacarousel/assets/js/wsacarousel_bootstrap4.0.js", array('version'=>'4.0.0'),
                 array('id'=>'wsacarousel_bootstrap.js', 'defer'=>'defer')); // defer .
 	    }
 	    
@@ -135,7 +135,7 @@ if($params->get('link_image',1) > 1) {
 	if($jquery) {
 	    $document->addScript(Uri::root(true).'/media/djextensions/magnific/magnific.js',  array('id'=>'magnific.js', 'defer'=>'defer'));
 		$document->addStyleSheet(Uri::root(true).'/media/djextensions/magnific/magnific.css');
-		$document->addScript(dirname(__FILE__).'/assets/js/magnific-init.js',  array('id'=>'magnific-init.js', 'defer'=>'defer'));
+		$document->addScript(Uri::root(true).'/modules/mod_wsacarousel/assets/js/magnific-init.js',  array('id'=>'magnific-init.js', 'defer'=>'defer'));
 	} 
 }
 
