acffaac1fed42ade921ac80a71c0abb42e5ad726
ahcwa <ahcwa@bram-len.fritz.box>
2022-03-14 16:37:31 +0100

    1.1.0

--- src/Helper/WsacarouselHelper.php
+++ src/Helper/WsacarouselHelper.php
@@ -449,7 +449,7 @@ class WsacarouselHelper
 		$pause = $params->get('pause_button');
 		
 		if(empty($play) || !file_exists(JPATH_ROOT.DS.$play)) $play = 'modules/mod_wsacarousel/themes/'.$theme.'/images/play.png';
-		if(empty($pause) || !file_exists(JPATH_ROOT.DS.$pause)) $pause = urlencode('data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause" viewBox="0 0 16 16">
+		if(empty($pause) || !file_exists(JPATH_ROOT.DS.$pause)) $pause = 'data:image/svg+xml;charset=UTF-8,' . urlencode('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause" viewBox="0 0 16 16">
   <path d="M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z"/>
 </svg>');
 		
