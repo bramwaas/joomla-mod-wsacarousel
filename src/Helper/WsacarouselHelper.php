acb856da2463d044dcd9e008011ff67bcba8e04e
ahcwa <ahcwa@bram-len.fritz.box>
2022-03-14 20:08:38 +0100

    1.1.0

--- src/Helper/WsacarouselHelper.php
+++ src/Helper/WsacarouselHelper.php
@@ -441,7 +441,7 @@ class WsacarouselHelper
 	        $next = $params->get('right_arrow');
 	        if(empty($prev) || !file_exists(JPATH_ROOT.DS.$prev)) $prev = 'data:image/svg+xml;charset=UTF-8,' . rawurlencode('<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="bi bi-chevron-left" viewBox="0 0 320 512">
 		  <path d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"/>
-            </svg>');
+          </svg>');
 	        if(empty($next) || !file_exists(JPATH_ROOT.DS.$next)) $next = 'data:image/svg+xml;charset=UTF-8,' . rawurlencode('<svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="bi bi-chevron-right" viewBox="0 0 320 512">
 		  <path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"/>
 		  </svg>');
