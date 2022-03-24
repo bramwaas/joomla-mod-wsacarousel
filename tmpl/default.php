ab3731db165704b6cb0e66d52bbfe6a79e29afdb
ahcwa <ahcwa@bram-len.fritz.box>
2022-03-24 14:28:01 +0100

    1.1.0

--- tmpl/default.php
+++ tmpl/default.php
@@ -105,7 +105,6 @@ float: left;
     align-items: center;
     justify-content: center;
 	width: " . 15/$style['vicnt'] . "%;
-    background-color: rgba(0, 0, 0, 0.1);
 	}
 #wsacarousel" . $mid . " .".  $carousel_class ."-indicators {
 	margin: 0 " . 15/$style['vicnt'] . "% 1rem;
@@ -151,12 +150,12 @@ right:  calc(100% - " . 100/$style['vicnt'] . "% + " . $style['marginr'] . ");
 	opacity: 0.9;
 }
 .carousel-control-prev {
-  background-image:         linear-gradient(to right, rgba(0, 0, 0, .5) 0%, rgba(0, 0, 0, .0001) 100%);
+  background-image:         linear-gradient(to right, rgba(0, 0, 0, .2) 0%, rgba(0, 0, 0, .0001) 100%);
   filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#80000000', endColorstr='#00000000', GradientType=1);
   background-repeat: repeat-x;
 }
 .carousel-control-next {
-  background-image:         linear-gradient(to right, rgba(0, 0, 0, .0001) 0%, rgba(0, 0, 0, .5) 100%);
+  background-image:         linear-gradient(to right, rgba(0, 0, 0, .0001) 0%, rgba(0, 0, 0, .2) 100%);
   filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#00000000', endColorstr='#80000000', GradientType=1);
   background-repeat: repeat-x;
 }
